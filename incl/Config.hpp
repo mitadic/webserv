f04a993047f7c7afb444af31a57ec3541d73d163
Petra Bencze <petra.bncz@gmail.com>
2025-02-24 16:47:26 +0100

    feat(config.cpp: in progress

--- incl/Config.hpp
+++ incl/Config.hpp
@@ -9,13 +9,15 @@
 
 # include "ServerBlock.hpp"
 
+# define DEFAULT_CONF "conf/default.conf"
+
 class Config {
     public:
         //std::string host;
-        static std::vector<ServerBlock> parse_config(std::string filename);
+        static void parse_config(std::string filename, std::vector<ServerBlock> & server_blocks);
 
     private:
-        std::stringstream _input;
+        std::stringstream   _input;
 };
 
 #endif
--- src/Config.cpp
+++ src/Config.cpp
@@ -1,2 +1,28 @@
 
 #include "Config.hpp"
+#include <fstream>
+
+void Config::parse_config(std::string filename, std::vector<ServerBlock> & server_blocks)
+{
+    std::ifstream   file(filename);
+    std::string     line;
+
+    if (!file.is_open())
+        throw std::runtime_error("couldn't open config file");
+    while (getline(file, line))
+    {
+        if (line.empty())
+            continue ;
+        else if (line == "server {")
+        {
+            ServerBlock block;
+            //parse block;
+            server_blocks.push_back(block);
+            if (line != "}")
+                throw std::runtime_error("in config file: missing closing braxcket '}'");
+        }
+        else
+            throw std::runtime_error("in config file: unexpected line");
+    }
+    file.close();
+}
