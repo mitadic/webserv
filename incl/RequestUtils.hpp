31beab0e7f831ce8a18f1dd6121254d4fe3ec3b2
Milos Tadic b550_Manj <milosgtadic@yahoo.com>
2025-03-17 18:10:00 +0100

    disable the hardcoded MAX_CONTENT_LENGTH

--- incl/RequestUtils.hpp
+++ incl/RequestUtils.hpp
@@ -20,7 +20,7 @@
 #define DOTS_OR_DIGITS ".1234567890"
 #define LOOPBACK_NUMERIC 2130706433
 #define MAX_URI_LENGTH 4096  // NGINX default
-#define MAX_CONTENT_LENGTH 10485760
+// #define MAX_CONTENT_LENGTH 10485760
 
 std::vector<std::string> split(const std::string& s, const std::string& delimiters);
 void    trim_lws(std::string& s);
