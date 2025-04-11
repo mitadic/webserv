0f2470d45fa8816d6dd60f563d88550bf65ab425
pebencze <petra.bncz@gmail.com>
2025-04-11 17:36:51 +0200

    note(tester): what to test on cgis

--- python_tester/test_cgi.py
+++ python_tester/test_cgi.py
@@ -13,3 +13,6 @@ def test_post_contact(webserver, secondary_url):
 	response = requests.post(f"{secondary_url}/cgi-bin/secondary_contact_form.py/guest-book", data={"name": "Sophie", "message": "Hello", "email:": "a@b.com" }, headers={"Content-Type": "text/plain"})
 	assert response.status_code == 201
 	assert "Form submitted" in response.text
+
+# curl -X POST http://127.0.0.1:8080/cgi-bin/secondary_upload_form.py \
+# -F "file=hello.txt"
