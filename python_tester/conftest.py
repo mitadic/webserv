e0146331466e2d37b72f9000ce8892806d221257
pebencze <petra.bncz@gmail.com>
2025-04-15 12:20:20 +0200

    feat(tester): valgrind with log file

--- python_tester/conftest.py
+++ python_tester/conftest.py
@@ -12,11 +12,30 @@ WEBSERV_PATH = "./a.out"
 @pytest.fixture(scope="session")
 def webserver():
 	"""Start the webserver for the entire test session"""
-	process = subprocess.Popen([WEBSERV_PATH])
+	valgrind_log = "python_tester/valgrind.log"
+	valgrind_command = [
+		"valgrind",
+		"--leak-check=full",
+		"--track-fds=yes",
+		"--log-file=" + valgrind_log,
+		WEBSERV_PATH
+	]
+	# process = subprocess.Popen([WEBSERV_PATH]) to run without valgrind
+	process = subprocess.Popen(valgrind_command) # to run with valgrind
 	time.sleep(2)  # Give server time to start
 	yield process # this is where the test will run
-	process.terminate()
-	process.wait()
+	process.terminate() # terminate the process
+	process.wait() # wait for it to finish
+
+	# Check the Valgrind log for leaks or errors
+	with open(valgrind_log, "r") as log_file:
+		valgrind_output = log_file.read()
+		print("\n[Valgrind Output]\n")
+		print(valgrind_output)
+
+		# Optionally, fail the test session if Valgrind detects issues
+		if "definitely lost: 0 bytes" not in valgrind_output or "All heap blocks were freed" not in valgrind_output:
+			pytest.fail("Valgrind detected memory leaks or file descriptor issues. Check valgrind.log for details.")
 
 # @pytest.fixture(scope="session")
 # def webserver_quickstart():
