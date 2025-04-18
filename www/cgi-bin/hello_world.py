ec1a2e7c33f53a3a646e7618656240e1f9a0bbb5
Milos Tadic b550_Manj <milosgtadic@yahoo.com>
2025-04-19 00:31:40 +0200

    feat(hello_world.py): [default disabled] signal handler to catch and ignore pipe-related printouts

--- www/cgi-bin/hello_world.py
+++ www/cgi-bin/hello_world.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# from signal import signal, SIGPIPE, SIG_DFL
+# signal(SIGPIPE, SIG_DFL)
+
+
 # this is a simple CGI script
 # a CGI script must include: content type, blank line, body
 
