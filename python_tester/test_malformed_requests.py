f2c8c2c9c426a5afc6d131ba78d32de5e9c866e9
Milos Tadic b550_Manj <milosgtadic@yahoo.com>
2025-04-20 01:10:13 +0200

    expand(test_malformed): add some 408 and 200

--- python_tester/test_malformed_requests.py
+++ python_tester/test_malformed_requests.py
@@ -2,6 +2,20 @@ import pytest
 import socket
 from urllib.parse import urlparse
 
+CODE_200 = [
+	# GET has body with length
+	("GET / HTTP/1.1\r\n"
+		"Content-Length: 3\r\n"
+		"\r\n"
+		"abc"),
+
+	# GET has body too long, this needs updating for multiple requests if pipelining tho
+	("GET / HTTP/1.1\r\n"
+		"Content-Length: 3\r\n"
+		"\r\n"
+		"abcdefghijklmnopqrstuvwxyz"),
+]
+
 MALFORMED_REQUESTS_400 = [
 
 	# double slash uri
@@ -43,6 +57,15 @@ MALFORMED_REQUESTS_400 = [
 
 	# incorrect content-length
 	# "POST / HTTP/1.1\r\nContent-Length: 4\r\n\r\nabc",
+
+	# port in Host too large
+	"GET / HTTP/1.1\r\nHost: localhost:70123\r\n\r\n"
+
+	# port in Host don't match?
+	"GET / HTTP/1.1\r\nHost: localhost:65222\r\n\r\n"
+
+	# bad Host number1
+	"GET / HTTP/1.1\r\nHost: 1277.0.0.1:8080\r\n\r\n"
 ]
 
 UNSUPPORTED_VERSIONS_505 = [
@@ -59,6 +82,27 @@ CODE_501 = [
 		"Content-Length: 3\r\n"
 		"\r\n"),
 ]
+
+TIMEOUT_408 = [
+
+	# barrage of nonsense without delimiter
+	"GET / HTTP/1.1\r\nHost: localhost:70123GET / HTTP/1.1\r\nHost: localhost:65222",
+
+	# nonsense length which never occurs
+	("GET / HTTP/1.1\r\n"
+		"Content-Length: 3\r\n"
+		"\r\n"),
+]
+
+@pytest.mark.parametrize("request_raw", CODE_200)
+def test_200(webserver, base_url, request_raw):
+	host = urlparse(base_url).hostname
+	port = urlparse(base_url).port
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((host, port))
+		s.sendall(request_raw.encode('utf-8'))
+		response = s.recv(4096).decode('utf-8')
+	assert "200" in response
 
 @pytest.mark.parametrize("request_raw", MALFORMED_REQUESTS_400)
 def test_400(webserver, base_url, request_raw):
@@ -89,3 +133,13 @@ def test_501(webserver, base_url, request_raw):
 		s.sendall(request_raw.encode('utf-8'))
 		response = s.recv(4096).decode('utf-8')
 	assert "501" in response
+
+# @pytest.mark.parametrize("request_raw", TIMEOUT_408)
+# def test_408(webserver, base_url, request_raw):
+# 	host = urlparse(base_url).hostname
+# 	port = urlparse(base_url).port
+# 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+# 		s.connect((host, port))
+# 		s.sendall(request_raw.encode('utf-8'))
+# 		response = s.recv(4096).decode('utf-8')
+# 	assert "408" in response
